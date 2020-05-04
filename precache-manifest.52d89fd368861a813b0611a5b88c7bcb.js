3ae0940601897d6d6e271908c0f9464884716a12
Serhii-P <serhii.pyskun@gmail.com>
2020-05-04 21:09:27 +0300

    Updates

---
+++ precache-manifest.52d89fd368861a813b0611a5b88c7bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee31e048e68b945d04d436719ae2e9ae",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "bfa79454b13ac700bd96",
+    "url": "/react_tabs-with-router/static/css/main.85a4bb90.chunk.css"
+  },
+  {
+    "revision": "f3e368cea3f4484b8d7c",
+    "url": "/react_tabs-with-router/static/js/2.14e4b1cc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react_tabs-with-router/static/js/2.14e4b1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa79454b13ac700bd96",
+    "url": "/react_tabs-with-router/static/js/main.93a5d79b.chunk.js"
+  },
+  {
+    "revision": "db4087bb12fceaa51b5e",
+    "url": "/react_tabs-with-router/static/js/runtime-main.7ad96a87.js"
+  }
+]);
